869a69a5ea1120827b3db76688dd5c9887ba6fd6
richardkello <kellorichard.gg@gmail.com>
2023-04-21 22:52:51 +0200

    Websocket communication and database adding

--- mtaa_test/consumer.py
+++ mtaa_test/consumer.py
@@ -1,6 +1,7 @@
 # chat/consumers.py
 import json
 
+from mtaa_test.models import Account, Room, Message
 from channels.generic.websocket import WebsocketConsumer
 
 
@@ -13,6 +14,16 @@ class ChatConsumer(WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        message_content = text_data_json["message"]
+        sender_id = text_data_json["sender_id"]
+        room_id = text_data_json["room_id"]
 
-        self.send(text_data=json.dumps({"message": message, }))
+        # Get the related Account and Room instances
+        account_instance = Account.objects.get(id=sender_id)
+        room_instance = Room.objects.get(id=room_id)
+
+        # Save the received message to the database
+        message_instance = Message(text=message_content, account=account_instance, room=room_instance)
+        message_instance.save()
+
+        self.send(text_data=json.dumps({"message": text_data_json, }))
